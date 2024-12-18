dd42812231c12e75e44645b9f81cee445f5c07cd
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2024-12-18 03:04:36 +0000

    remove dead comment

--- eleventy.config.js
+++ eleventy.config.js
@@ -28,7 +28,7 @@ export default function(eleventyConfig) {
   eleventyConfig.addGlobalData("eleventyComputed", {
     permalink: (data) => {
       if (data.page.inputPath.match(/[0-9]{4}/)) {
-        // Customize the permalink format here /{{ page.date | date: '%Y/%b' }}/
+        // Permalink format for posts: /<year>/<short month>/
         const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(data.page.date);
         const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(data.page.date);
         return `/${year}/${month}/`;
@@ -36,18 +36,6 @@ export default function(eleventyConfig) {
       return data.permalink; // Preserve any existing permalink or default
     },
   });
-
-	/*eleventyConfig.addDateParsing(function(dateValue) {
-    this:
-    page: {
-        inputPath: './_posts/2024/2024-10-01-October.html',
-        fileSlug: 'October',
-        filePathStem: '/_posts/2024/October',
-        outputFileExtension: 'html',
-        templateSyntax: 'liquid'
-    }
-		return false;
-	});*/
 
     return config;
 };
