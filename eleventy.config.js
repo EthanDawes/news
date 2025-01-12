8c7496c3ca87f0cb840a4435bd69d23fb0f5e24d
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2025-01-12 03:52:18 +0000

    liquid process mjml

--- eleventy.config.js
+++ eleventy.config.js
@@ -24,13 +24,14 @@ export default function(eleventyConfig) {
 
   // https://www.11ty.dev/docs/languages/custom/
   eleventyConfig.addExtension("mjml", {
-		compile: async (inputContent) => {
-			const output = mjml2html(inputContent).html;
+		key: "liquid",
+	});
 
-			return async () => {
-				return output;
-			};
-		},
+  eleventyConfig.addTransform("mjml", function (content) {
+		if (this.page.inputPath.endsWith(".mjml"))
+      return mjml2html(content).html;
+
+		return content; // no changes made.
 	});
 
   // https://www.11ty.dev/docs/collections-api/#getfilteredbyglob(-glob-)
